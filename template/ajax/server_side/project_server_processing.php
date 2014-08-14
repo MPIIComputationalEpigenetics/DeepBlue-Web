5a1b3fb3397d610f657d26d4b214f2cbd0d0a6fc
Umidjon Urunov <s9umurun@stud.uni-saarland.de>
2014-08-14 20:11:28 +0000

    List of projects implemented

---
+++ template/ajax/server_side/project_server_processing.php
@@ -0,0 +1,39 @@
+<?php
+
+//include IXR Library for RPC-XML
+require_once("../../lib/deepblue.IXR_Library.php");
+
+$url = 'http://deepblue.mpi-inf.mpg.de/xmlrpc';
+$user_key = 'JBv8qZORmuNr7G6N';
+
+        /* Getting data from the server */
+
+        $client = new IXR_Client($url);
+        
+        if(!$client->query("list_projects", $user_key)){
+            $projectList[] = 'An error occured - '.$client->getErrorCode()." : ".$client->getErrorMessage();
+        }
+        else{
+            $client->query("list_projects", $user_key);
+            $projectList[] = $client->getResponse();
+        }
+
+        /* Ordering and generating json file for Datatables */
+             
+        $orderedDataStr = array();
+        $tempArr = array();
+
+        foreach($projectList[0][1] as $orderedData){
+
+                $tempArr[] = $orderedData[0];
+                $tempArr[] = $orderedData[1];
+
+                array_push($orderedDataStr, $tempArr);
+
+                $tempArr = array();
+        }
+
+        echo json_encode(array('data' => $orderedDataStr));
+
+
+?>
