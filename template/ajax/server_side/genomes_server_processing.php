e4c592c98a80920f78c6b941bed5aba6007217b3
Umidjon Urunov <s9umurun@stud.uni-saarland.de>
2014-08-20 22:01:30 +0000

    Genome List :: Added additional columns

--- template/ajax/server_side/genomes_server_processing.php
+++ template/ajax/server_side/genomes_server_processing.php
@@ -18,19 +18,28 @@ require_once("../../lib/lib.php");
             $genomeList[] = $client->getResponse();
         }
 
+        foreach ($genomeList[0][1] as $value) {
+            $client->query("info", $value[0], $user_key);
+            $infoList[] = $client->getResponse();
+        }
+
         /* Ordering and generating json file for Datatables */
+
 
         $orderedDataStr = array();
         $tempArr = array();
 
-        foreach($genomeList[0][1] as $orderedData){
+        foreach($infoList as $orderedData){
+            foreach ($orderedData as $key_2 => $value_2) {
+                if($key_2 != 'okay'){
+                    $tempArr[] = $value_2['_id'];
+                    $tempArr[] = $value_2['name'];
+                    $tempArr[] = $value_2['description'];
+                }
+            }
 
-                $tempArr[] = $orderedData[0];
-                $tempArr[] = $orderedData[1];
-
-                array_push($orderedDataStr, $tempArr);
-
-                $tempArr = array();
+            array_push($orderedDataStr, $tempArr);
+            $tempArr = array();
         }
 
         echo json_encode(array('data' => $orderedDataStr));
