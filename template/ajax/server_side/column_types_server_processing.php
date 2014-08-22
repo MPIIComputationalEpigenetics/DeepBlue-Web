f54501737efc6babd6aaf528c90cbd9e97288602
Umidjon Urunov <s9umurun@stud.uni-saarland.de>
2014-08-22 13:40:56 +0000

    Column Types List :: Edited after feedback

--- template/ajax/server_side/column_types_server_processing.php
+++ template/ajax/server_side/column_types_server_processing.php
@@ -1,46 +1,66 @@
 <?php
 
+/**
+*   DeepBlue Epigenomic Data Server
+*   Copyright (c) 2014 Max Planck Institute for Computer Science.
+*   All rights reserved.
+*
+*   Authors :
+*
+*   Felipe Albrecht <felipe.albrecht@mpi-inf.mpg.de>
+*   Umidjon Urunov <umidjon.urunov@mpi-inf.mpg.de>
+*
+*   Created : 21-08-2014
+*
+*   ================================================
+*
+*   File : column_types_server_processing.php
+*
+*/
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 //include IXR Library for RPC-XML
 require_once("../../lib/deepblue.IXR_Library.php");
 
 /* Including URL for server and USER Key  */
 require_once("../../lib/lib.php");
 
-        /* Getting data from the server */
+/* Getting data from the server */
 
-        $client = new IXR_Client($url);
+$client = new IXR_Client($url);
 
-        if(!$client->query("list_column_types", $user_key)){
-            $columnList[] = 'An error occured - '.$client->getErrorCode()." : ".$client->getErrorMessage();
-        }
-        else{
-            $client->query("list_column_types", $user_key);
-            $columnList[] = $client->getResponse();
-        }
+if(!$client->query("list_column_types", $user_key)){
+    $columnList[] = 'An error occured - '.$client->getErrorCode()." : ".$client->getErrorMessage();
+}
+else{
+    $client->query("list_column_types", $user_key);
+    $columnList[] = $client->getResponse();
+}
 
-        /* Ordering and generating json file for Datatables */
+/* Ordering and generating json file for Datatables */
 
-        $orderedDataStr = array();
+$orderedDataStr = array();
+$tempArr = array();
+
+foreach($columnList[0][1] as $orderedData){
+
+    $splitIntoArrays[] = explode(" ", $orderedData[1]);
+
+    foreach ($splitIntoArrays as $val_1) {
+
+        $tempArr[] = $orderedData[0];
+        $tempArr[] = str_replace("'", "", $val_1[3]);
+        $tempArr[] = str_replace("'", "", $val_1[8]);
+
+        array_push($orderedDataStr, $tempArr);
         $tempArr = array();
+    }
 
-        foreach($columnList[0][1] as $orderedData){
+    $splitIntoArrays = array();
+}
 
-            $splitIntoArrays[] = explode(" ", $orderedData[1]);
-
-                foreach ($splitIntoArrays as $val_1) {
-
-                    $tempArr[] = $orderedData[0];
-                    $tempArr[] = str_replace("'", "", $val_1[3]);
-                    $tempArr[] = str_replace("'", "", $val_1[8]);
-
-                    array_push($orderedDataStr, $tempArr);
-                    $tempArr = array();
-                }
-
-            $splitIntoArrays = array();
-        }
-
-        echo json_encode(array('data' => $orderedDataStr));
-
+echo json_encode(array('data' => $orderedDataStr));
 
 ?>
