ce48e45c7eaf4e5de4ac3780fb684dd66e865a5b
Umidjon Urunov <s9umurun@stud.uni-saarland.de>
2014-08-22 13:53:44 +0000

    Projects List :: Edited after feedback

--- template/ajax/server_side/projects_server_processing.php
+++ template/ajax/server_side/projects_server_processing.php
@@ -1,39 +1,72 @@
 <?php
 
+/**
+*   DeepBlue Epigenomic Data Server
+*   Copyright (c) 2014 Max Planck Institute for Computer Science.
+*   All rights reserved.
+*
+*   Authors :
+*
+*   Felipe Albrecht <felipe.albrecht@mpi-inf.mpg.de>
+*   Umidjon Urunov <umidjon.urunov@mpi-inf.mpg.de>
+*
+*   Created : 21-08-2014
+*
+*   ================================================
+*
+*   File : projects_server_processing.php
+*
+*/
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 //include IXR Library for RPC-XML
 require_once("../../lib/deepblue.IXR_Library.php");
 
 /* Including URL for server and USER Key  */
 require_once("../../lib/lib.php");
 
-        /* Getting data from the server */
+/* Getting data from the server */
 
-        $client = new IXR_Client($url);
+$client = new IXR_Client($url);
 
-        if(!$client->query("list_projects", $user_key)){
-            $projectList[] = 'An error occured - '.$client->getErrorCode()." : ".$client->getErrorMessage();
-        }
-        else{
-            $client->query("list_projects", $user_key);
-            $projectList[] = $client->getResponse();
-        }
+if(!$client->query("list_projects", $user_key)){
+    $projectList[] = 'An error occured - '.$client->getErrorCode()." : ".$client->getErrorMessage();
+}
+else{
+    $client->query("list_projects", $user_key);
+    $projectList[] = $client->getResponse();
+}
 
-        /* Ordering and generating json file for Datatables */
+/* Collecting projects ids into array */
+$projectIds = array();
 
-        $orderedDataStr = array();
-        $tempArr = array();
+foreach ($projectList[0][1] as $project) {
+    $projectIds[] = $project[0];
+}
 
-        foreach($projectList[0][1] as $orderedData){
+/* Getting info data about projects */
 
-                $tempArr[] = $orderedData[0];
-                $tempArr[] = $orderedData[1];
+$client->query("info", $projectIds, $user_key);
+$infoList[] = $client->getResponse();
 
-                array_push($orderedDataStr, $tempArr);
+/* Ordering and generating json file for Datatables */
 
-                $tempArr = array();
-        }
+$orderedDataStr = array();
+$tempArr = array();
 
-        echo json_encode(array('data' => $orderedDataStr));
+foreach($infoList[0][1] as $orderedData){
 
+    $tempArr[] = $orderedData['_id'];
+    $tempArr[] = $orderedData['name'];
+    $tempArr[] = $orderedData['description'];
+
+    array_push($orderedDataStr, $tempArr);
+
+    $tempArr = array();
+}
+
+echo json_encode(array('data' => $orderedDataStr));
 
 ?>
