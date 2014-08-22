4e54cb7f616a6f60e0d9238adcde52070af878f5
Umidjon Urunov <s9umurun@stud.uni-saarland.de>
2014-08-22 01:12:14 +0000

    Techniques List :: Edited after feedback

--- template/ajax/server_side/techniques_server_processing.php
+++ template/ajax/server_side/techniques_server_processing.php
@@ -1,39 +1,69 @@
 <?php
 
+/**
+*   DeepBlue Epigenomic Data Server
+*   Copyright (c) 2014 Max Planck Institute for Computer Science.
+*   All rights reserved.
+*
+*   Authors :
+*
+*   Felipe Albrecht <felipe.albrecht@mpi-inf.mpg.de>
+*   Umidjon Urunov <umidjon.urunov@mpi-inf.mpg.de>
+*
+*   Created : 21-08-2014
+*
+*   ================================================
+*
+*   File : techniques_server_processing.php
+*
+*/
+
 //include IXR Library for RPC-XML
 require_once("../../lib/deepblue.IXR_Library.php");
 
 /* Including URL for server and USER Key  */
 require_once("../../lib/lib.php");
 
-        /* Getting data from the server */
+/* Getting data from the server */
 
-        $client = new IXR_Client($url);
+$client = new IXR_Client($url);
 
-        if(!$client->query("list_techniques", $user_key)){
-            $techList[] = 'An error occured - '.$client->getErrorCode()." : ".$client->getErrorMessage();
-        }
-        else{
-            $client->query("list_techniques", $user_key);
-            $techList[] = $client->getResponse();
-        }
+if(!$client->query("list_techniques", $user_key)){
+    $techList[] = 'An error occured - '.$client->getErrorCode()." : ".$client->getErrorMessage();
+}
+else{
+    $client->query("list_techniques", $user_key);
+    $techList[] = $client->getResponse();
+}
 
-        /* Ordering and generating json file for Datatables */
+/* Collecting epigenetic mark ids into array */
+$techniquesIds = array();
 
-        $orderedDataStr = array();
-        $tempArr = array();
+foreach ($techList[0][1] as $techniques) {
+    $techniquesIds[] = $techniques[0];
+}
 
-        foreach($techList[0][1] as $orderedData){
+/* Getting info data about epigenetc marks */
 
-                $tempArr[] = $orderedData[0];
-                $tempArr[] = $orderedData[1];
+$client->query("info", $techniquesIds, $user_key);
+$infoList[] = $client->getResponse();
 
-                array_push($orderedDataStr, $tempArr);
+/* Ordering and generating json file for Datatables */
 
-                $tempArr = array();
-        }
+$orderedDataStr = array();
+$tempArr = array();
 
-        echo json_encode(array('data' => $orderedDataStr));
+foreach($infoList[0][1] as $orderedData){
 
+    $tempArr[] = $orderedData['_id'];
+    $tempArr[] = $orderedData['name'];
+    $tempArr[] = $orderedData['description'];
+
+    array_push($orderedDataStr, $tempArr);
+
+    $tempArr = array();
+}
+
+echo json_encode(array('data' => $orderedDataStr));
 
 ?>
