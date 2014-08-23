74ba25d9a2864f4c378bb2f21396b03debecdd50
Felipe Albrecht <felipe.albrecht@gmail.com>
2014-08-23 19:22:16 +0200

    Fix for loading all the data

--- template/ajax/server_side/bio_sources_server_processing.php
+++ template/ajax/server_side/bio_sources_server_processing.php
@@ -19,6 +19,8 @@ require_once("../../lib/lib.php");
 //include IXR Library for RPC-XML
 require_once("../../lib/deepblue.IXR_Library.php");
 
+ini_set('memory_limit', '-1');
+
 $client = new IXR_Client($url);
 
 if(!$client->query("list_bio_sources", $user_key)){
@@ -41,25 +43,22 @@ $infoList[] = $client->getResponse();
 /* Ordering and generating json file for Datatables */
 
 $orderedDataStr = array();
-$tempArr = array();
-$tempBioStr = "";
+
 
 foreach($infoList[0][1] as $orderedData){
+    $tempArr = array();
 
     $tempArr[] = !isset($orderedData['_id']) ? "" : $orderedData['_id'];
     $tempArr[] = !isset($orderedData['name']) ? "" : $orderedData['name'];
     $tempArr[] = !isset($orderedData['description']) ? "" : $orderedData['description'];
 
-
+    $tempBioStr = "";
     foreach ($orderedData['extra_metadata'] as $bioKey => $bioValue) {
         $tempBioStr .= '<b>'.$bioKey.'</b> : '.$bioValue.'<br/>';
     }
-
     $tempArr[] = $tempBioStr;
 
-    array_push($orderedDataStr, $tempArr);
-    $tempArr = array();
-    $tempBioStr = "";
+    $orderedDataStr[] = $tempArr;
 }
 
 echo json_encode(array('data' => $orderedDataStr));
