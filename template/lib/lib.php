35db739e0efb6d6a7ee2951d5652531463cb9c18
Umidjon Urunov <s9umurun@stud.uni-saarland.de>
2014-08-23 17:59:34 +0000

    lib/lib.php :: Changed

--- template/lib/lib.php
+++ template/lib/lib.php
@@ -2,9 +2,10 @@
 
 /* URL to XML-RPC Server */
 $url = 'http://deepblue.mpi-inf.mpg.de/xmlrpc';
+//$url = '139.19.102.157:31415';
 
 /* USER Key */
 $user_key = 'JBv8qZORmuNr7G6N';
-
+//$user_key = 'j68NShtvcInNj0x7';
 
 ?>
