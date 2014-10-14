85a653f386e949f05636c67802e448bfc8ac26e4
Felipe Albrecht <felipe.albrecht@gmail.com>
2014-10-14 11:02:20 +0200

    Remove useless line

--- template/ajax/server_side/biosources_server_processing.php
+++ template/ajax/server_side/biosources_server_processing.php
@@ -22,7 +22,6 @@ require_once("../../lib/deepblue.IXR_Library.php");
 ini_set('memory_limit', '-1');
 set_time_limit(12000);
 
-$client = new IXR_Client($url, $timeout = 120000);
 $client = new IXR_Client($url);
 
 if(!$client->query("list_biosources", $user_key)){
