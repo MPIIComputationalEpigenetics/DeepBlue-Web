e0fd5648da240233cc205dee7b4f6c95b4406637
Felipe Albrecht <felipe.albrecht@gmail.com>
2014-10-14 09:55:20 +0200

    new user key

--- template/lib/lib.php
+++ template/lib/lib.php
@@ -19,10 +19,10 @@ ini_set('display_errors', 1);
 
 /* URL to XML-RPC Server */
 $url = 'http://deepblue.mpi-inf.mpg.de/xmlrpc';
-//$url = '139.19.102.157:31415';
+//$url = 'http://localhost:31415';
 
 /* USER Key */
-$user_key = 'JBv8qZORmuNr7G6N';
-//$user_key = 'j68NShtvcInNj0x7';
+//$user_key = 'JBv8qZORmuNr7G6N';
+$user_key = 'hQy5V2ii3X0J3hQl';
 
 ?>
