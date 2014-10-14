aca04a3321081d9f461bbd1fc60251f75801b5f1
Felipe Albrecht <felipe.albrecht@gmail.com>
2014-10-14 09:55:34 +0200

    code clean up

--- template/ajax/server_side/annotations_server_processing.php
+++ template/ajax/server_side/annotations_server_processing.php
@@ -13,18 +13,10 @@
 *   Created : 21-08-2014
 */
 
-/* DeepBlue Configuration */
-require_once("../../lib/lib.php");
-
-/* include IXR Library for RPC-XML */
-require_once("../../lib/deepblue.IXR_Library.php");
-
-/* DeepBlue Class */
 require_once("../../lib/deepblue.functions.php");
+
 $deepBlueObj = new Deepblue();
 
-//$client = new IXR_Client($url);
-
-$deepBlueObj->annotationDataTable('', '');
+print_r($deepBlueObj->annotationDataTable('', ''));
 
 ?>
