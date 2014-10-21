fa5eb0c914817c04ca6d175262c3e0b76607b4e8
Felipe Albrecht <felipe.albrecht@gmail.com>
2014-10-21 23:27:09 +0200

    FIxed dynamic nested list

--- template/ajax/deepblue_view_biosources_hierarchy.php
+++ template/ajax/deepblue_view_biosources_hierarchy.php
@@ -40,27 +40,39 @@
 
 					<!-- widget content -->
 					<div class="widget-body">
-						<div class="well">
 						 	<div class="row show-grid">
 								<div class="col-md-6">
 									<h3>Please, select the desired BioSources</h3>
 									<div class="input-group">
 	  									<span class="input-group-addon">@</span>
-										<input type="text" value="" class="form-control" id="user_biosource" placeholder="Search biosource by name" />
+										<input type="text" value="" class="form-control" id="user_biosource" placeholder="Search Biosource" />
 									</div>
 									<div id="biosources-tree"> </div>
 								</div>
 								<div class="col-md-6 col-md-offset-0">
 									<h3>Selected BioSouces</h3>
-									<ul class="list-group" id="selected-biosources">
-									</ul>
+									<div class="dd" id="selected-biosources-nestable">
+										<ol class="dd-list" id="selected-biosources"> </ol>
+									</div>
 								</div>
 							</div>
-						</div>
+
+
+
+
+
 						<script>
 						var tree_div = $('#biosources-tree');
 						var no_dulicates = true;
 						var li_id_to_biosource = {};
+						var selected_biosources = [];
+						var biosource_to_samples = {};
+
+						var data_id_next = 1;
+						function get_data_id() {
+							var v = data_id_next++;
+							return v.toString();
+						}
 
 						function is_in(name, count, sons, exists) {
 							if (exists.indexOf(name) >= 0) {
@@ -118,100 +130,82 @@
 							}
 						}
 
+						function insert_sample(id, biosource_name) {
+							selected_biosources[biosource_name] = id;
+							var request = $.ajax({
+								url: "ajax/server_side/samples_server_processing.php",
+								dataType: "json",
+								data : {
+									biosources: biosource_name
+								}
+							});
+
+							request.done( function(data) {
+								$.each(data.data, function (index, value) {
+									var node_id = "#" +id + '-list-group-item-sub';
+									var title = "Sample ID : " + value[0];
+
+									var content = '<ol class="dd-list"><li class="dd-item" data-id="' + value[0]+"-li'><div class='dd-handle bg-color-blue txt-color-white'><i>" + value[2] + "</i></div></li></ol>";
+
+									$(node_id).append('<li class="dd-item" data-id="' +get_data_id()+ '"> <div class="dd-handle">'+title+'</div> </li>');
+								});
+							});
+
+							request.fail( function(jqXHR, textStatus) {
+								console.log(jqXHR);
+			        			console.log('Error: '+ textStatus);
+								alert( textStatus);
+							});
+						}
+
+						function remove_sample(biosource_name) {
+							var index = selected_biosources.indexOf(biosource_name);
+							if (index  > -1) {
+								selected_biosources.splice(index, 1);
+							}
+						}
+
 						function select_node(e, data) {
 							var id = data.node.id;
 							var biosource = li_id_to_biosource[id];
 							var name =  biosource.name;
 							var count = biosource.count;
 							if (count > 0) {
+								insert_sample(id, name);
 								var id = id + '-list-group-item';
-           						$('#selected-biosources').append('<li id='+id+' class="list-group-item">'+name+'<span class="badge">'+count+'</span></li>');
+								var id_sub = id + "-sub";
+           						$('#selected-biosources').append('<li id="'+id+'" class="dd-item" data-id="'+get_data_id()+'"><div class="dd-handle">'+name+'<em class="pull-right badge" data-placement="left">'+count+'</em></div><ol class="dd-list" id="'+id_sub+'"></ol></li>');
+
+           						$('#selected-biosources-nestable').nestable('setParent', $('#'+id));
            					}
            					$.each(data.node.children_d, function (index, id_son) {
            						biosource = li_id_to_biosource[id_son];
            						var count = biosource.count;
            						if (count > 0) {
            							var name =  biosource.name;
+           							insert_sample(id_son, name);
            							var id = id_son + '-list-group-item';
-           							$('#selected-biosources').append('<li id='+id+' class="list-group-item">'+name+'<span class="badge">'+count+'</span></li>');
+           							var id_sub = id + "-sub";
+           							$('#selected-biosources').append('<li id="'+id+'" data-id="'+get_data_id()+'" class="dd-item"><div class="dd-handle">'+name+'<em class="pull-right badge" data-placement="left" >'+count+'</em></div><ol class="dd-list" id="'+id_sub+'"></ol></li>');
+           							$('#selected-biosources-nestable').nestable('setParent', $('#'+id));
            						}
            					});
            				}
 
            				function deselect_node(e, data) {
 							var id = data.node.id;
+							biosource = li_id_to_biosource[id];
+							remove_sample(biosource.name);
 							var id = '#' + id + '-list-group-item';
            					$(id).remove();
+
            					$.each(data.node.children_d, function (index, id_son) {
+           						biosource = li_id_to_biosource[id_son];
+								remove_sample(biosource.name);
            						var id_son = "#" + id_son + "-list-group-item";
            						$(id_son).remove();
            					});
            				}
-
-						var request = $.ajax({
-							url: "ajax/server_side/biosources_tree.php",
-							dataType: "json",
-						});
-
-						request.done( function(data) {
-							exists = [];
-							$.each(data.data, function (index, value) {
-								create_root(tree_div, value[0], value[1], value[2], value[3], "", exists);
-							});
-
-							$('#biosources-tree').jstree({
-  								"core" : {
-							    "animation" : 1,
-							    "check_callback" : true,
-							    "themes" : { "stripes" : true },
-							    "open_parents": true,
-							  },
-							  "types" : {
-							    "#" : {
-							      "max_children" : 1,
-							      "max_depth" : 4,
-							      "valid_children" : ["root"]
-							    },
-							    "root" : {
-							      "icon" : "/static/3.0.6/assets/images/tree_icon.png",
-							      "valid_children" : ["default"]
-							    },
-							    "default" : {
-							      "valid_children" : ["default","file"]
-							    },
-							    "file" : {
-							      "icon" : "glyphicon glyphicon-file",
-							      "valid_children" : []
-							    }
-							  },
-							  "plugins" : [
-							    "unique","search", "state", "types", "wholerow", "checkbox", "sort"
-							  ]
-							})
-							.bind("select_node.jstree", function (e, data) {
-								select_node(e, data);
-        					})
-        					.bind("deselect_node.jstree", function (e, data) {
-        						deselect_node(e, data);
-        					});
-
-							var to = false;
-							$('#user_biosource').keyup(function () {
-								if(to) { clearTimeout(to); }
-								to = setTimeout(function () {
-									var v = $('#user_biosource').val();
-									$('#biosources-tree').jstree(true).search(v);
-								}, 250);
-							});
-
-        				});
-
-						request.fail( function(jqXHR, textStatus) {
-							console.log(jqXHR);
-			        		console.log('Error: '+ textStatus);
-							alert( "error" );
-						});
-
 
 					</script>
 
@@ -245,10 +239,77 @@
 
 	pageSetUp();
 
-	var pagefunction = function() {
-		loadScript("js/plugin/jstree.js");
-	};
+	function build_tree() {
+			var request = $.ajax({
+			url: "ajax/server_side/biosources_tree.php",
+			dataType: "json",
+		});
 
-	pagefunction();
+		request.done( function(data) {
+			exists = [];
+			$.each(data.data, function (index, value) {
+				create_root(tree_div, value[0], value[1], value[2], value[3], "", exists);
+			});
+
+			$('#biosources-tree').jstree({
+					"core" : {
+			    "animation" : 1,
+			    "check_callback" : true,
+			    "themes" : { "stripes" : true },
+			    "open_parents": true,
+			  },
+			  "types" : {
+			    "#" : {
+			      "max_children" : 1,
+			      "max_depth" : 4,
+			      "valid_children" : ["root"]
+			    },
+			    "root" : {
+			      "icon" : "/static/3.0.6/assets/images/tree_icon.png",
+			      "valid_children" : ["default"]
+			    },
+			    "default" : {
+			      "valid_children" : ["default","file"]
+			    },
+			    "file" : {
+			      "icon" : "glyphicon glyphicon-file",
+			      "valid_children" : []
+			    }
+			  },
+			  "plugins" : [
+			    "unique","search", "state", "types", "wholerow", "checkbox", "sort"
+			  ]
+			})
+			.bind("select_node.jstree", function (e, data) {
+				select_node(e, data);
+			})
+			.bind("deselect_node.jstree", function (e, data) {
+				deselect_node(e, data);
+			});
+
+			var to = false;
+			$('#user_biosource').keyup(function () {
+				if(to) { clearTimeout(to); }
+				to = setTimeout(function () {
+					var v = $('#user_biosource').val();
+					$('#biosources-tree').jstree(true).search(v);
+				}, 250);
+			});
+
+		});
+
+		request.fail( function(jqXHR, textStatus) {
+			console.log(jqXHR);
+			console.log('Error: '+ textStatus);
+			alert( "error" );
+		});
+	}
+
+	loadScript("js/plugin/jquery-nestable/jquery.nestable.js", function () {
+		loadScript("js/plugin/jstree.js", function() {
+			build_tree();
+			$('#selected-biosources-nestable').nestable({group : 1});
+		});
+	});
 
 </script>
