d58fcb6554009bb717844c665e4e373eefa81981
Obaro Odiete <edeas123@gmail.com>
2014-12-15 18:42:24 +0100

    Dashboard Charts

--- template/ajax/dashboard.php
+++ template/ajax/dashboard.php
@@ -7,11 +7,11 @@
 *
 *   Felipe Albrecht <felipe.albrecht@mpi-inf.mpg.de>
 *   Umidjon Urunov <umidjon.urunov@mpi-inf.mpg.de>
-*		Obaro Odiete <s8obodie@stud.uni-saarland.de>
+*	Obaro Odiete <s8obodie@stud.uni-saarland.de>
 *
 *   Created : 25-08-2014
 *
-*		Last Updated : 07-12-2014
+*	Last Updated : 07-12-2014
 *
 -->
 
@@ -19,6 +19,10 @@
 
 	/* DeepBlue Configuration */
 	require_once("../lib/lib.php");
+	
+	/* DeepBlue Class */
+	require_once("../lib/deepblue.functions.php");
+	$deepBlueObj = new Deepblue();	
 	
 	/* include IXR Library for RPC-XML */
 	require_once("../lib/deepblue.IXR_Library.php");
@@ -300,6 +304,34 @@
 </section>
 <!-- end widget grid -->
 
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="modelSearchExperimentInfo" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+          &times;
+        </button>
+        <h4 class="modal-title" id="modalSearchExperimenentInfo">Experiment Info</h4>
+      </div>
+      <div class="modal-body custom-scroll terms-body">
+        <div id='modal_for_experiment' style="display:none;"></div>
+        <div id="modal-content-by-jquery" style="display:none;">
+          <?php echo $deepBlueObj->experimentDataTableTemplate("search"); ?>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" id="downloadExperimentButton" class="btn btn-primary download-btn-size">
+          Download
+        </button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">
+          Close
+        </button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
 <script type="text/javascript">
 	
 	/* DO NOT REMOVE : GLOBAL FUNCTIONS!
@@ -336,16 +368,172 @@
 	
 	// manage clicks
 	$('#projects-chart').bind("plotclick", function (event, pos, item) {
-        if (item) { 
-            alert(item.series.label);
+        if (item) {
+            lauchModalView(item.series.label, 'project');            
         }
     });
 
 	$('#techniques-chart').bind("plotclick", function (event, pos, item) {
         if (item) { 
-            alert(item.series.label);
+            lauchModalView(item.series.label, 'technique');            
         }
     });
+
+    // MODAL VIEW
+    
+    function lauchModalView(label, vocab) {
+
+		$('.modal-content').removeClass( "modalViewSingleInfo" );
+		$('#modal_for_experiment').hide();
+		$('#modal-content-by-jquery').show();  
+
+		var type = vocab;
+		var text = label;
+		
+		// there was a special case to handle SAMPLES
+
+		/* Hide and show experiment metadata */
+		var isShow = false;
+		$(document).on("click", '.exp-metadata-more-view', function () {
+			//var metadata = $(this).prev();
+			if(isShow == false){
+				$(this).prev().show(1000);
+				$(this).text("-- Hide --");
+				isShow = true;
+			}
+			else{
+				$(this).prev().hide(1000);
+				$(this).text("-- View metadata --");
+				isShow = false;
+			}
+
+		});
+
+		/* Reset all input values */
+
+		$('.hasinput input').val("");
+		$('.hasinput input').prop('disabled', false);
+
+		/* BASIC */
+		var responsiveHelper_dt_basic = undefined;
+		var responsiveHelper_datatable_fixed_column = undefined;
+		var responsiveHelper_datatable_col_reorder = undefined;
+		var responsiveHelper_datatable_tabletools = undefined;
+
+		var breakpointDefinition = {
+			tablet : 1024,
+			phone : 480
+		};
+
+		var selectedElementsModal = [];
+		/* COLUMN FILTER  */
+
+		var otable = $('#datatable_fixed_column').DataTable({
+
+		    "ajax": {
+		    "url": "ajax/server_side/modal_view_server_processing.php",
+		    "data": function ( d ) {
+		       		d.types = type;
+		        	d.titles = text;
+		       	}
+		    },
+		    "iDisplayLength": 50,
+		    "autoWidth" : true,
+		    "bDestroy": true,
+			"preDrawCallback" : function() {
+				// Initialize the responsive datatables helper once.
+				if (!responsiveHelper_datatable_fixed_column) {
+					responsiveHelper_datatable_fixed_column = new ResponsiveDatatablesHelper($('#datatable_fixed_column'), breakpointDefinition);
+				}
+			},
+			"rowCallback" : function(nRow) {
+				responsiveHelper_datatable_fixed_column.createExpandIcon(nRow);
+			},
+			"drawCallback" : function(oSettings) {
+				responsiveHelper_datatable_fixed_column.respond();
+			},
+			"fnInitComplete": function(oSettings, json) {
+
+				var inputName;
+
+				switch (type) {
+					case 'experiment':
+					    //alert('This is experiment');
+					    break;
+					case 'annotation':
+					    //alert('Annotations');
+					    break;
+					case 'genome':
+					   	inputName = '#experiment-genome';
+					    break;
+					case 'epigenetic_mark':
+					    inputName = '#experiment-epigenetic_mark';
+					    break;
+					case 'sample':
+					    inputName = '#experiment-sample';
+					    break;
+					case 'technique':
+					    inputName = '#experiment-technique';
+					    break;
+					case 'project':
+					    inputName = '#experiment-project';
+					    break;
+					default:
+					    break;
+				}
+				
+				$(inputName).val(text);
+				$(inputName).prop('disabled', true);
+
+				/* Insert or remove selected or unselected elements */
+				//selectedElementsModal = [];
+				$( ".downloadCheckBox" ).change(function() {
+					var downloadIdModal = $(this).parent().next().text();
+					var downloadTitleModal = $(this).parent().next().next().text();
+					var downloadTotalModal = downloadIdModal+"-"+downloadTitleModal;
+
+					var foundModal = $.inArray(downloadTotalModal, selectedElementsModal);
+
+					if(foundModal < 0){
+						selectedElementsModal.push(downloadTotalModal);
+					}
+					else{
+						selectedElementsModal.splice(foundModal, 1);
+					}
+				});
+
+			}
+
+		});
+
+
+		// custom toolbar
+		$("div.toolbar").html('<div class="text-right"><img src="img/logo.png" alt="DeepBlue" style="width: 111px; margin-top: 3px; margin-right: 10px;"></div>');
+
+		// Apply the filter
+		$("#datatable_fixed_column thead th input[type=text]").on( 'keyup change', function () {
+		    otable
+		        .column( $(this).parent().index()+':visible' )
+		        .search( this.value )
+		        .draw();
+
+		});
+
+		/* Download button :: Getting selected elements */
+		$('#downloadBtnTop').click(function(){
+			if(selectedElementsModal.length == 0){
+				//alert("( Modal ) Please select elements!");
+				//alert("( length = 0 ) Klikkkkk");
+			}
+			else{
+				//alert(selectedElementsModal);
+				//alert("( length != 0 ) Klikkkkk");
+			}
+
+		});
+		
+    	$('#myModal').modal('toggle');
+	}    
 		
 	// PAGE RELATED SCRIPTS
 	var pagefunction = function() {
@@ -395,8 +583,7 @@
 		if ($('#projects-chart').length) {
 				var placeholder = "#projects-chart";
 				var data = <?php echo json_encode($list['projects']);?>;
-				var option = pie_option;				
-
+				var option = pie_option;
 				$.plot($(placeholder), data, option);
 		}
 		/* end projects pie chart */
@@ -536,6 +723,13 @@
 	});
 	
 
-	
-	
+	loadScript("js/plugin/datatables/jquery.dataTables.min.js", function(){
+		loadScript("js/plugin/datatables/dataTables.colVis.min.js", function(){
+			loadScript("js/plugin/datatables/dataTables.tableTools.min.js", function(){
+				loadScript("js/plugin/datatables/dataTables.bootstrap.min.js", function(){
+					loadScript("js/plugin/datatable-responsive/datatables.responsive.min.js", pagefunction)
+				});
+			});
+		});
+	});
 </script>
