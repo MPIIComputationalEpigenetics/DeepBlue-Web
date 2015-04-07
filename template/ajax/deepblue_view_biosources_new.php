c6b5f62c85068214725a0d561c66a3566860d290
Felipe Albrecht <felipe.albrecht@gmail.com>
2015-04-07 19:09:36 +0200

    Biosources

---
+++ template/ajax/deepblue_view_biosources_new.php
@@ -0,0 +1,193 @@
+<?php
+
+/**
+*   DeepBlue Epigenomic Data Server
+*   Copyright (c) 2015 Max Planck Institute for Computer Science.
+*   All rights reserved.
+*
+*   Authors :
+*
+*   Felipe Albrecht <felipe.albrecht@mpi-inf.mpg.de>
+*
+*   Created : 07-04-2014
+*
+*   ================================================
+*
+*   File : deepblue_view_biosources.php
+*
+*/
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require_once("inc/init.php");
+
+?>
+<div class="row">
+	<div class="col-xs-12 col-sm-7 col-md-7 col-lg-4">
+		<h1 class="page-title txt-color-blueDark">
+			<i class="fa fa-table fa-fw "></i>
+				Data Tables > BioSources
+			</span>
+		</h1>
+	</div>
+</div>
+
+<!-- widget grid -->
+<section id="widget-grid" class="">
+
+	<!-- row -->
+	<div class="row">
+
+		<!-- NEW WIDGET START -->
+		<article class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+
+
+			<!-- Widget ID (each widget will need unique ID)-->
+			<div class="jarviswidget jarviswidget-color-blueDark" id="datatable-biosource" data-widget-editbutton="false" data-widget-deletebutton="false" data-widget-colorbutton="false" data-widget-togglebutton="false">
+
+				<header>
+					<span class="widget-icon"> <i class="fa fa-table"></i> </span>
+					<h2>BioSources</h2>
+
+				</header>
+
+				<div>
+
+					<!-- widget edit box -->
+					<div class="jarviswidget-editbox">
+						<!-- This area used as dropdown edit box -->
+
+					</div>
+					<!-- end widget edit box -->
+
+					<!-- widget content -->
+					<div class="widget-body no-padding">
+
+						<table id="datatable_fixed_column" class="table table-striped table-bordered" width="100%">
+
+					        <thead>
+								<tr>
+									<th class="hasinput">
+										<input class="form-control" placeholder="ID" type="text"/>
+									</th>
+									<th class="hasinput">
+										<input type="text" class="form-control" placeholder="BioSource"/>
+									</th>
+									<th class="hasinput">
+										<input type="text" class="form-control" placeholder="Descrpition"/>
+									</th>
+									<th class="hasinput">
+										<input type="text" class="form-control" placeholder="Metadata"/>
+									</th>
+								</tr>
+					            <tr>
+				                    <th>ID</th>
+				                    <th>BioSources</th>
+				                    <th>Description</th>
+				                    <th>Metadata</th>
+					            </tr>
+					        </thead>
+
+						</table>
+
+					</div>
+					<!-- end widget content -->
+
+				</div>
+				<!-- end widget div -->
+
+			</div>
+			<!-- end widget -->
+
+		</article>
+		<!-- WIDGET END -->
+
+	</div>
+
+	<!-- end row -->
+
+	<!-- end row -->
+
+</section>
+<!-- end widget grid -->
+
+<script type="text/javascript">
+
+	pageSetUp();
+
+
+	// pagefunction
+	var pagefunction = function() {
+
+		/* BASIC ;*/
+			var responsiveHelper_dt_basic = undefined;
+			var responsiveHelper_datatable_fixed_column = undefined;
+			var responsiveHelper_datatable_col_reorder = undefined;
+			var responsiveHelper_datatable_tabletools = undefined;
+
+			var breakpointDefinition = {
+				tablet : 1024,
+				phone : 480
+			};
+
+		/* COLUMN FILTER  */
+	    var otable = $('#datatable_fixed_column').DataTable({
+
+	    	"bServerSide": true,
+	        "sAjaxSource": "api/datatable",
+	        "fnServerParams": function ( aoData ) {
+      			aoData.push( { "name": "collection", "value": "biosources" } );
+      			aoData.push( { "name": "col_0", "value": "_id"} );
+      			aoData.push( { "name": "col_1", "value": "name"} );
+      			aoData.push( { "name": "col_2", "value": "description"} );
+      			aoData.push( { "name": "col_3", "value": "extra_metadata"} );
+    		},
+	        //"sServerMethod": "POST",
+	        "iDisplayLength": 50,
+	        "autoWidth" : true,
+			"preDrawCallback" : function() {
+				// Initialize the responsive datatables helper once.
+				if (!responsiveHelper_datatable_fixed_column) {
+					responsiveHelper_datatable_fixed_column = new ResponsiveDatatablesHelper($('#datatable_fixed_column'), breakpointDefinition);
+				}
+			},
+			"rowCallback" : function(nRow) {
+				responsiveHelper_datatable_fixed_column.createExpandIcon(nRow);
+			},
+			"drawCallback" : function(oSettings) {
+				responsiveHelper_datatable_fixed_column.respond();
+			}
+
+	    });
+
+	    // custom toolbar
+	    $("div.toolbar").html('<div class="text-right"><img src="img/logo.png" alt="DeepBlue" style="width: 111px; margin-top: 3px; margin-right: 10px;"></div>');
+
+	    // Apply the filter
+	    $("#datatable_fixed_column thead th input[type=text]").on( 'keyup change', function () {
+
+	        otable
+	            .column( $(this).parent().index()+':visible' )
+	            .search( this.value )
+	            .draw();
+
+	    } );
+	    /* END COLUMN FILTER */
+	};
+
+	// load related plugins
+
+	loadScript("js/plugin/datatables/jquery.dataTables.min.js", function(){
+		loadScript("js/plugin/datatables/dataTables.colVis.min.js", function(){
+			loadScript("js/plugin/datatables/dataTables.tableTools.min.js", function(){
+				loadScript("js/plugin/datatables/dataTables.bootstrap.min.js", function(){
+					loadScript("js/plugin/datatable-responsive/datatables.responsive.min.js", pagefunction)
+				});
+			});
+		});
+	});
+
+
+</script>
