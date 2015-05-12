77d537883eadcd995df42fd7fee57fc611caa98b
Felipe Albrecht <felipe.albrecht@gmail.com>
2015-05-12 16:46:06 +0200

    Register

--- template/php/deepblue-register.php
+++ template/php/deepblue-register.php
@@ -1,10 +1,11 @@
 	<?php
-	$str = "";
+	$str = "New User: \n";
 	foreach ($_POST as $key => $value) {
 		$str .= $key . " " . $value . "\n";
 	}
 
-	mail("felipe.albrect@gmail.com", "New User", $str);
+	mail("felipe.albrecht@gmail.com", "New User", $str);
+
 
 	print "<h1> Thank you for your interest on DeepBlue Epigenomic Data Server.</h1>";
 	print "We will check your information and create an account for you.";
