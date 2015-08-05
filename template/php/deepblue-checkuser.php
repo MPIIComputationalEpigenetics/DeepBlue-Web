7380aaa7ee8f60e427b062208c9ec7430547890b
Felipe Albrecht <felipe.albrecht@gmail.com>
2015-08-05 12:27:57 +0200

    Fixes

--- template/php/deepblue-checkuser.php
+++ template/php/deepblue-checkuser.php
@@ -6,32 +6,31 @@
 
 /* include IXR Library for RPC-XML */
 require_once("../lib/deepblue.IXR_Library.php");
-include("../lib/lib.php");
+include("../lib/server_settings.php");
 
-//$url = 'http://deepblue.mpi-inf.mpg.de/xmlrpc';
-$client = new IXR_Client($url);
+$client = new IXR_Client(get_server());
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
 if(!$client->query("user_auth", $email, $password)){
-	die('An error occurred - '.$client->getErrorCode().":".$client->getErrorMessage());	
+	die('An error occurred - '.$client->getErrorCode().":".$client->getErrorMessage());
 }
 
 $response = $client->getResponse();
 
 if ($response[0] == 'error') {
-	header("Location: ../index.php?login_attempt=1");	
+	header("Location: ../index.php?login_attempt=1");
 }
 else {
 	$user_key = $response[1];
 	if(!$client->query("info", "me", $user_key)) {
-		die('An error occurred - '.$client->getErrorCode().":".$client->getErrorMessage());	
+		die('An error occurred - '.$client->getErrorCode().":".$client->getErrorMessage());
 	}
 
 	$response = $client->getResponse();
 	$user_details = $response[1][0];
-	
+
 	session_start();
 
 	$_SESSION['user_email'] = $user_details['email'];
