33177621a16944b1bc8844c07bb644fe316c0f3c
edeas123 <edeas123@gmail.com>
2015-08-20 17:07:13 +0200

    Copyright

--- template/inc/footer.php
+++ template/inc/footer.php
@@ -2,7 +2,7 @@
 <div class="page-footer">
 	<div class="row">
 		<div class="col-xs-12">
-			<span class="txt-color-white hidden-xs pull-right text-left">DeepBlue Epigenomic Data Server © 2013-2014</span>
+			<span class="txt-color-white hidden-xs pull-right text-left">Max Planck Institute for Computer Science © 2015</span>
 		</div>
 	</div>
 </div>
