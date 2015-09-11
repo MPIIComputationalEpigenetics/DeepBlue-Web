e8a264f6ab03d513f7b6b6cc9c9cb4ad46143c63
edeas123 <edeas123@gmail.com>
2015-09-11 22:41:40 +0200

    Add acknowledgements page #41

---
+++ template/ajax/deepblue_acknowledgements.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+*   DeepBlue Epigenomic Data Server
+*   Copyright (c) 2014 Max Planck Institute for Computer Science.
+*   All rights reserved.
+*
+*   File : deepblue_acknowledgements.php
+*
+*   Felipe Albrecht <felipe.albrecht@mpi-inf.mpg.de>
+*
+*   Created : 10-09-2015
+*/
