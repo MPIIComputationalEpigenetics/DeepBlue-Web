9a1e9b14301a6182bc81ece447c69462dbfb2277
Felipe Albrecht <felipe.albrecht@gmail.com>
2015-09-21 14:12:13 +0200

    Acknowledgements page

--- template/ajax/deepblue_acknowledgements.php
+++ template/ajax/deepblue_acknowledgements.php
@@ -11,3 +11,59 @@
 *
 *   Created : 10-09-2015
 */
+
+require_once("inc/init.php");
+?>
+
+<div class="row">
+	<div class="col-sm-6">
+		<div class="bs-example bs-example-type">
+			<h2 class="text-primary" style="margin: 20px 0;">Acknowledgements</h3>
+			<table class="table" style="background-color: #fafafa;">
+				<tbody>
+					<tr>
+						<td>
+							<h3 class="text-primary">Concept, design, and main development</h3>
+							<p class="lead">Felipe Albrecht</p>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<h3 class="text-primary">Advisors</h3>
+							<p class="lead">Prof. Dr. Dr. Thomas Lengauer</p>
+							<p class="lead">Dr. Christoph Bock</p>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<h3 class="text-primary">Developers</h3>
+							<p class="lead">Fabian Reinartz - Server (C++) and data inclusion (python) - 06.2014 to 03.2014</p>
+							<p class="lead">Umidjon Urunov - Web Interface (php, html, javascript, css) - 08.2014 to 11.2014</p>
+							<p class="lead">Natalie Wirth - Server (C++) and data inclusion (python) - 12.2014 to 07.2015</p>
+							<p class="lead">Obaro Odiete - Web Interface (php, html, javascript, css) - 08.2014 to 11.2014</p>
+						</td>
+					</tr>
+
+					<tr>
+						<td>
+							<h3 class="text-primary">Supporters</h3>
+							<p class="lead"><a href="http://mpi-inf.mpg.de">Max Planck Institute for Computer Science</a></p>
+							<p class="lead"><a href="http://deutsches-epigenom-programm.de">DEEP - DEutsches Epigenom Programm</a></p>
+							<p class="lead"><a href="http://www.blueprint-epigenome.eu">BLUEPRINT - A BLUEPRINT of Haematopoietic Epigenomes</a></p>
+
+							<p class="lead">This work has been supported by German Science Ministry Grant No. 01KU1216A (DEEP project) and has been performed in the context of EU grant no. HEALTH-F5-2011-282510 (BLUEPRINT project)</p>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+
+		</div>
+	</div>
+</div>
+
+
+
+
+
+
+
