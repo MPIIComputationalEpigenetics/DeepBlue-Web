c82b0bb476a3a1578c3ed492fdab5d6ce3b82700
Felipe Albrecht <felipe.albrecht@gmail.com>
2015-10-02 11:30:07 +0200

    Better features page

--- template/features.php
+++ template/features.php
@@ -24,8 +24,21 @@ $page_body_prop = array("id"=>"extr-page");
 include("inc/header.php");
 
 ?>
-<!-- ==========================CONTENT STARTS HERE ========================== -->
-<!-- possible classes: minified, no-right-panel, fixed-ribbon, fixed-header, fixed-width-->
+
+<style type="text/css">
+@media (min-width: 768px) {
+    .dl-horizontal dt {
+        width:280px;
+        white-space: normal;
+    }
+    .dl-horizontal dd {
+        margin-left:300px;
+        margin-bottom: 12px;
+    }
+}
+
+</style>
+
 <header id="header">
 	<!--<span id="logo"></span>-->
 
@@ -39,9 +52,11 @@ include("inc/header.php");
 
 	<div class="navbar-collapse collapse">
     	<ul class="nav navbar-nav navbar-left">
-			<li><span id="extr-page-header-space"><a href="features.php">Features List</a></span></li>
+			<li><span id="extr-page-header-space"><a href="features.php">Features</a></span></li>
+        	<li><span id="extr-page-header-space"><a href="operations.php">Operations</a></span></li>
         	<li><span id="extr-page-header-space"><a href="manual">Manual</a></span></li>
         	<li><span id="extr-page-header-space"><a href="api.php">API Reference</a></span></li>
+        	<li><span id="extr-page-header-space"><a href="tutorials.php">Tutorials</a></span></li>
     	</ul>
   	</div>
 
@@ -51,37 +66,56 @@ include("inc/header.php");
 	<!-- MAIN CONTENT -->
 	<div id="content" class="container">
 		<div class="row">
-			<h1> DeepBlue Epigenomic Data Server Features: </h1>
-			<ul>
-				<li>DeepBlue contains the Methylation (WGBS, RRBS, and Infinium 450k), Histone Modifications and Variants, DNaseI, Transcription Factors Binding Sites, Chromatin State Segmetation, and gene expression (mRNA-seq) datasets from the major epigenome consortia: ENCODE, Roadmap Epigenomic, and BLUEPRINT Epigenomics</li>
-				<li>Update module that retrieves the latest epigenome datasets from the major epigenome consortia</li>
-				<li>Access via an XML-RPC protocol that is supported by all major programming languages</li>
-				<li>Web Interface (in development)</li>
-				<li>User data upload facility</li>
-				<li>Analysis operations performed directly on the data server</li>
-				<li>Reproducibility by automatic documenting and storing of the analysis steps</li>
-				<li>Controlled vocabulary to ensure data consistency</li>
-				<li>Full text search command, expanded for all experiments metadata and controlled vocabularies</li>
-				<li>BioSources data hierarchy and synonyms</li>
-				<li>Flexible data model for storing experiments metadata</li>
-				<li>Data Compression</li>
-				<li>Data scalability through data distribution across several compute nodes (sharding), provided by MongoDB</li>
-				<li>Distributed processing</li>
-				<li>A set of operations for working with the data (see below)</li>
-			</ul>
-			<h1>Data Operations</h1>
-			<ul>
-				<li>Select epigenomic data from pre-installed datasets or datasets uploaded by the user</li>
-				<li>Filter epigenomic data by metadata</li>
-				<li>Filter epigenomic data by region attributes</li>
-				<li>Find overlapping regions sets</li>
-				<li>Group regions</li>
-				<li>Retrieve DNA sequences</li>
-				<li>DNA pattern matching operations</li>
-				<li>Aggregate and summarize regions</li>
-				<li>Count regions</li>
-				<li>Output in BED format with desired columns</li>
-			</ul>
+
+			<h1 style="margin-bottom: 25px;"> DeepBlue Features</h1>
+			<dl class="dl-horizontal" id="freelance">
+				<dt>Thousand datasets</dt>
+				<dd>DNA Methylation, Histone modifications and variants, DNaseI, Transcription Factors Binding Sites, Chromatin State Segmentation, and gene expression (mRNA) datasets from the major epigenome consortia: <i>ENCODE</i>, <i>ROADMAP Epigenomic</i>, <i>BLUEPRINT Epigenome</i>, and <i>DEEP</i>.</dd>
+
+				<dt>Operations for epigenomic data</dt>
+				<dd>Select, filter, summarize, transform, and download the epigenomic data. See some examples in our <a href="operations.php">operations</a> page.</dd>
+
+				<dt>Peaks and signal data experiments</dt>
+				<dd>DeepBlue contains the peaks (<i>bed</i>) and signals (<i>wig</i>, <i>bedgraph</i>) datasets. It is virtually possible to import into DeepBlue any file that is based on genomic region.</dd>
+
+				<dt>Auxiliary annotations</dt>
+				<dd>Genes, promoters, CpG island, transcripts, and more. You are welcome and encouraged to upload their own annotations.</dd>
+
+				<dt>Language-agnostic</dt>
+				<dd>The access is made via an XML-RPC protocol that is supported by all major programming languages.</dd>
+
+				<dt>Metadata consistency</dt>
+				<dd>Every experiment has 5 mandatory fields (<i>genome</i>, <i>epigenetic mark</i>, <i>sample</i>, <i>technique</i>, and <i>project</i>) that are registered with their metadata into their collections for data consistency.</dd>
+
+				<dt>Flexible metadata model</dt>
+				<dd>DeepBlue ensure the consistency through the mandatory fields (<i>genome</i>, <i>epigenetic mark</i>, <i>sample</i>, <i>technique</i>, and <i>project</i>) are used to ensure the data consistency. More information about the data can be included using the <i>extra-metadata</i>, a key-value container, where any information about the data can be stored and searched.</dd>
+
+				<dt>Names from ontologies</dt>
+				<dd>DeepBlue imports the biological source names  (cell lines, cell types, tissues, organs, and others) from the CL, EFO, and UBERON ontologies. The terms synonyms and terms hierarchy are also imported.</dd>
+
+				<dt>Full text search</dt>
+				<dd>DeepBlue full text search is expanded to all experiments metadata and controlled vocabularies. The samples information, genomes, epigenetic marks, techniques, experiments, and annotations metadata are indexed and searchable by full text input.</dd>
+
+				<dt>Upload your own data</dt>
+				<dd>You can upload your own experimental and annotation data.</dd>
+
+				<dt>Private experimental data</dt>
+				<dd>You can create your own private projects, where the experiments stored there are only visible to you and invited colleagues.</dd>
+
+				<dt>Processing in ours servers</dt>
+				<dd>The operations are performed directly on the data server. You only have to download the final result.</dd>
+
+				<dt>Reproducing analysis steps</dt>
+				<dd>The data operations are stored in the server and can be re-executed and shared with colleagues. DeepBlue automatically documents the steps that were executed during the analysis.</dd>
+
+				<dt>Web Interface (in development)</dt>
+				<dd>You can browse, search, and download the data included into DeepBlue using our user friendly interface.
+				</dd>
+
+				<dt>Anonymous access</dt>
+				<dd>You are not obligated to create an account. You can access DeepBlue data and its mean features without having to register.
+				</dd>
+			</dl>
 		</div>
 	</div>
 
