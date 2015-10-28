eec83b3f37ed23f192dca61d00fd026778b5872e
Felipe Albrecht <felipe.albrecht@gmail.com>
2015-10-28 16:59:24 +0100

    remove useless lib

--- template/inc/scripts.php
+++ template/inc/scripts.php
@@ -21,9 +21,6 @@
 		<!-- IMPORTANT: APP CONFIG -->
 		<script src="js/app.config.js"></script>
 
-		<!-- JS TOUCH : include this plugin for mobile drag / drop touch events-->
-		<script src="<?php echo ASSETS_URL; ?>/js/plugin/jquery-touch/jquery.ui.touch-punch.min.js"></script>
-
 		<!-- BOOTSTRAP JS -->
 		<script src="<?php echo ASSETS_URL; ?>/js/bootstrap/bootstrap.min.js"></script>
 
