a37fa98651f0595b888a8801fcb7bb9e0f04820d
Felipe Albrecht <felipe.albrecht@gmail.com>
2015-11-02 21:46:53 +0100

    improvments in the check_error

--- template/lib/error.php
+++ template/lib/error.php
@@ -12,9 +12,14 @@
 */
 
 function check_error($response) {
-	$error = $response[0] == "error";
-	if ($error) {
-		list($code, $msg) = explode(":", $response[1]);
+	if ($response[0] == "error") {
+		$error_msg = $response[1];
+		if (empty($error_msg)) {
+			echo json_encode(array("error" => "Unknown error.", "code" => "unknown", "message" => "An unknown error happened in DeepBlue. Please, inform us (deepblue@mpi-inf.mpg.de about this error. Thank you for the comprehension."));
+			die;
+		}
+		echo "is in\n";
+		list($code, $msg) = explode(":", $error_msg);
 		echo json_encode(array("error" => $response[1], "code" => $code, "message" => $msg));
 		die;
 	}
