f9d1ccc5eb9aff1cdc57660993bf12c17f7d1e27
Felipe Albrecht <felipe.albrecht@gmail.com>
2015-12-17 15:16:33 +0100

    English fixes main page

--- template/features.php
+++ template/features.php
@@ -48,47 +48,49 @@ include("inc/header.php");
 
 			<h1 style="margin-bottom: 25px;"> DeepBlue Features</h1>
 			<dl class="dl-horizontal" id="freelance">
-				<dt>Thousand datasets</dt>
-				<dd>DNA Methylation, Histone modifications and variants, DNaseI, Transcription Factors Binding Sites, Chromatin State Segmentation, and gene expression (mRNA) datasets from the major epigenome consortia: <i>ENCODE</i>, <i>ROADMAP Epigenomic</i>, <i>BLUEPRINT Epigenome</i>, and <i>DEEP</i>.</dd>
+				<dt>Thousand of datasets</dt>
+				<dd>DNA Methylation, Histone Modifications and Variants, DNaseI, Transcription Factors Binding Sites, Chromatin State Segmentation, and Gene Expression (mRNA) data from the major epigenome consortia: <i>ENCODE</i>, <i>ROADMAP Epigenomic</i>, <i>BLUEPRINT Epigenome</i>, and <i>DEEP</i>.</dd>
 
 				<dt>Operations for epigenomic data</dt>
-				<dd>Select, filter, summarize, transform, and download the epigenomic data. See some examples in our <a href="examples.php">examples</a> page.</dd>
+				<dd>Select, filter, summarize, transform, and download the epigenomic data. See some examples on our <a href="examples.php">examples</a> page.</dd>
 
 				<dt>Peaks and signal data experiments</dt>
-				<dd>DeepBlue contains the peaks (<i>bed</i>) and signals (<i>wig</i>, <i>bedgraph</i>) datasets. It is virtually possible to import into DeepBlue any file that is based on genomic region.</dd>
+				<dd>DeepBlue contains the peaks (<i>bed</i>) and signals (<i>wig</i>, <i>bedgraph</i>) datasets. It is possible to import virtually any file that is based on genomic region into DeepBlue.</dd>
 
 				<dt>Auxiliary annotations</dt>
-				<dd>Genes, promoters, CpG island, transcripts, and more. You are welcome and encouraged to upload their own annotations.</dd>
+				<dd>Genes, promoters, CpG island, transcripts, and more. You are welcome and encouraged to upload their respective annotations.</dd>
 
-				<dt>Language-agnostic</dt>
+				<dt>Language agnostic</dt>
 				<dd>The access is made via an XML-RPC protocol that is supported by all major programming languages.</dd>
 
 				<dt>Metadata consistency</dt>
-				<dd>Every experiment has 5 mandatory fields (<i>genome</i>, <i>epigenetic mark</i>, <i>sample</i>, <i>technique</i>, and <i>project</i>) that are registered with their metadata into their collections for data consistency.</dd>
+				<dd>Every experiment has 5 mandatory fields (<i>genome</i>, <i>epigenetic mark</i>, <i>sample</i>, <i>technique</i>, and <i>project</i>) that are registered with their metadata in their collections for data consistency.</dd>
 
 				<dt>Flexible metadata model</dt>
-				<dd>DeepBlue ensure the consistency through the mandatory fields (<i>genome</i>, <i>epigenetic mark</i>, <i>sample</i>, <i>technique</i>, and <i>project</i>) are used to ensure the data consistency. More information about the data can be included using the <i>extra-metadata</i>, a key-value container, where any information about the data can be stored and searched.</dd>
+				<dd>DeepBlue ensures the metadata consistency through the mandatory fields (<i>genome</i>, <i>epigenetic mark</i>, <i>sample</i>, <i>technique</i>, and <i>project</i>). More information about the data can be included using the <i>extra-metadata</i>, a key-value container, where any information about the data can be stored and searched.</dd>
 
 				<dt>Names from ontologies</dt>
-				<dd>DeepBlue imports the biological source names  (cell lines, cell types, tissues, organs, and others) from the CL, EFO, and UBERON ontologies. The terms synonyms and terms hierarchy are also imported.</dd>
+				<dd>DeepBlue imports the biological source names  (cell lines, cell types, tissues, organs, and others) from the CL, EFO, and UBERON ontologies. The terms' synonyms and hierarchies are imported together with the terms' names.</dd>
 
 				<dt>Full text search</dt>
-				<dd>DeepBlue full text search is expanded to all experiments metadata and controlled vocabularies. The samples information, genomes, epigenetic marks, techniques, experiments, and annotations metadata are indexed and searchable by full text input.</dd>
+				<dd>DeepBlue full text search is expanded to all experiments' metadata and controlled vocabularies.
+				The samples' information, genomes, epigenetic marks, techniques, experiments, and annotations metadata are indexed and searchable by full text input.</dd>
 
 				<dt>Upload your own data</dt>
 				<dd>You can upload your own experimental and annotation data.</dd>
 
 				<dt>Private experimental data</dt>
-				<dd>You can create your own private projects, where the experiments stored there are only visible to you and invited colleagues.</dd>
+				<dd>You can create your own private projects, where the stored experiments there are only visible to you and invited colleagues.</dd>
 
 				<dt>Processing in ours servers</dt>
-				<dd>The operations are performed directly on the data server. You only have to download the final result.</dd>
+				<dd>The operations are performed directly on the data server. You need only download the final result.</dd>
 
 				<dt>Reproducing analysis steps</dt>
-				<dd>The data operations are stored in the server and can be re-executed and shared with colleagues. DeepBlue automatically documents the steps that were executed during the analysis.</dd>
+				<dd>The data operations are stored in the server and can be re-executed and shared with colleagues.
+				DeepBlue automatically documents all steps executed during the analysis.</dd>
 
 				<dt>Web Interface</dt>
-				<dd>You can browse, search, and download the data included into DeepBlue using our user friendly interface.
+				<dd>You can browse, search, and download the data included into DeepBlue using our user-friendly interface.
 				</dd>
 
 				<dt>Anonymous access</dt>
