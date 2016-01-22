54515f64ae41400b7033d06b93ef4d1a8bbd6c91
Felipe Albrecht <felipe.albrecht@gmail.com>
2016-01-22 02:16:12 +0100

    Improvements...

--- template/supplementary_material_nar_webservices_2016.php
+++ template/supplementary_material_nar_webservices_2016.php
@@ -27,14 +27,14 @@ include("inc/header.php");
 <style type="text/css">
 @media (min-width: 768px) {
     .dl-horizontal dt {
-        width:340px;
+        width:450px;
         padding-right: 12px;
         white-space: normal;
         text-align: justify;
         text-justify: inter-word;
     }
     .dl-horizontal dd {
-        margin-left:340px;
+        margin-left:450px;
         margin-bottom: 12px;
     }
 }
@@ -73,7 +73,7 @@ include("inc/header.php");
         <h3>Operations examples</h3>
         <p class="lead" style="text-align: justify;">We provide a list of examples in the <a href="http://deepblue.mpi-inf.mpg.de/examples.php" target="_blank">DeepBlue API - examples</a> page.</p>
 
-        <div class="navy-line"></div>
+        <div style="width: 550px" class="navy-line"></div>
 
         <h3>Use cases</h3>
 
@@ -107,11 +107,105 @@ include("inc/header.php");
             <br />
             <br />
             <br />
+            <p>We import the necessary libraries and assign to the variable <i>server</i> an instance to a proxy to the DeepBlue. <small>(lines 1 - 7)</small></p>
+            <p>After, we test the connection to the server. The expected output is <i>['okay', 'DeepBlue (1.6.5) says hi to anonymous']</i>. <small>(line 8)</small></p>
             <br />
-            <p>We import the necessary libraries and assign to the variable <i>server</i> an instance to a proxy to the DeepBlue. <small>(lines 1 - 10)</small></p>
+            <br />
+            <p>We list and extract all samples IDs with the biosource <i>H1-hESC</i> from the <i>ENCODE</i> project. <small>(lines 10-13)</small></p>
             <br />
             <br />
-            <p>After, we test the connection to the server. The expected output is <i>['okay', 'DeepBlue (1.6.5) says hi to anonymous']</i>. <small>(line 11)</small></p>
+            <br />
+            <p>We list all peaks experiments that contains the previously selected samples IDS, the histone modification H3K4me3 from the <i>ENCODE</i> project.<small>(line 16)</small></p>
+            <br />
+            <br />
+            <p>After, we extract the IDs from the listed experiments, obtain information about the experiment using the ID, and generate a list of experiments that the original file name ends with "bed.gz". <small>(lines 21-24)</small></p>
+            <p>As we are interested only on one experiment file, we do a check if found only one experiment. <small>(lines 25-26)</small></p>
+            <br />
+            <br />
+            <br />
+            <p>We select the regions of the selected h1-hESC H3K4me3 experiment. <small>(line 30)</small>
+            <br />
+            <br />
+            <br />
+            <br />
+            <p>Then, we list and extract the names of the <i>DNA Methylation</i> experiments that contains <i>liver</i> or <i>hepatocyte</i> biosource. <small>(lines 35-40)</small></p>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <p>We iterate in each selected <i>DNA Methylation</i> experiment, selecting its regions. <small>(lines 47-52)</small></p>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <p>We aggregate the selected regions using the <i>H1-hESC</i> regions.  We perform the aggregation on the column '<i>SCORE</i>'. <small>(lines 55-56)</small></p>
+            <br />
+            <br />
+            <p>We filter and remove the aggregated regions whom did not aggregated any region. <small>(lines 59-60)</small></p>
+            <br />
+            <br />
+            <p>Finally, we request the regions with the desired columns. We store the experiment name with the associated request ID, and also the request ID in a list of IDs. <small>(line 63-67)</small>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <p> We create an directory to store the data download from DeepBlue. <small>(lines 72-73)</small>
+            <br />
+            <br />
+            <br />
+            <p>The data is retrieved later. Were each request status is verified. If the request is <i>done</i>, its data is downloaded, stored in a file, and the request is removed from the list. It is repeated until all requests are processed. <small>(lines 77-90)</small>
+
+          </dt>
+          <dd>
+            <script src="https://gist.github.com/felipealbrecht/f81c14d2a52e9543567c.js"></script>
+          </dd>
+          <div style="width: 550px" class="navy-line"></div>
+
+          <!-- -->
+          <h4 id='example-search_experiments'>Use case 2:<br/><b>Identification of H3k27ac peaks that overlap with promoters in all BLUEPRINT datasets and subsequent identification of transcription factor peaks that overlap with these promoters, on all ENCODE datasets</b></h4>
+          <dt style="width:70%">
+          <dt style="width:70%">
+            <p class="lead" style="text-align: justify; padding: 15px;">
+              We select the promoters regions and the H3K27ac peaks from BLUEPRINT.</p>
+            </p>
+            <p class="lead" style="text-align: justify; padding: 15px;">
+              We filter the H3K27ac regions that overlap with some promoter region.</p>
+            <p>
+            <p class="lead" style="text-align: justify; padding: 15px;">
+              We select the signal of three Transcription Factors (TF): AG01, AG02, and AG03.</p>
+            <p>
+            <p class="lead" style="text-align: justify; padding: 15px;">
+              We filter the TFs regions that overlap with some filtered H3K27ac peaks.</p>
+            <p>
+            <p class="lead" style="text-align: justify; padding: 15px;">
+              Finally, we request the regions and download them.
+            <p>
+          </dt>
+          </dt>
+          <dd>
+            <div align="right" style="padding: 10px">
+              <img class="img-responsive" style="align: right; width: 40%; border: 1px solid #ccc" src="https://www.lucidchart.com/publicSegments/view/003debf9-865b-4928-9a19-7600d51a2a0c/image.png">
+            </div>
+          </dd>
+
+          <h4 style="padding: 10px 0px 20px 0px " id='example-search_experiments'><b>Source code</b>  <small>(Copy it into your python interpreter)</small></h4>
+          <dt>
+            <h4 id='example-search_experiments'>Identification of H3k27ac peaks that overlap with promoters in all BLUEPRINT datasets and subsequent identification of transcription factor peaks that overlap with these promoters, on all ENCODE datasets</h4>
+            <br />
+            <br />
+            <p>We import the necessary libraries and assign to the variable <i>server</i> an instance to a proxy to the DeepBlue. <small>(lines 1 - 9)</small></p>
             <br />
             <br />
             <p>We list and extract all samples IDs with the biosource <i>H1-hESC</i> from the <i>ENCODE</i> project. <small>(lines 15-)</small></p>
@@ -141,31 +235,8 @@ include("inc/header.php");
             <br />
             <br />
             <br />
-            <p>The data is retrieved later. Were each request status is verified. If the request is <i>done</i>, its data is downloaded, stored in a file, and the request is removed from the list. It is repeated until all requests are processed. <small>(lines 60-75)</small>
+            <p>The data is retrieved later. Were each request status is verified. If the request is <i>done</i>, its data is downloaded, stored in a file, and the request is removed from the list. It is repeated until all requests are processed. <small>(lines 60-74)</small>
 
-          </dt>
-          <dd>
-            <script src="https://gist.github.com/felipealbrecht/f81c14d2a52e9543567c.js"></script>
-          </dd>
-
-          <!-- -->
-          <h4 id='example-search_experiments'>Use case 2:<br/><b>Identification of H3k27ac peaks that overlap with promoters in all BLUEPRINT datasets and subsequent identification of transcription factor peaks that overlap with these promoters, on all ENCODE datasets</b></h4>
-          <dt style="width:70%">
-            <p class="lead" style="text-align: justify; padding: 15px;">
-            blabla
-            </p>
-          </dt>
-          <dd>
-            <div align="right" style="padding: 10px">
-              <img class="img-responsive" style="align: right; width: 40%; border: 1px solid #ccc" src="https://www.lucidchart.com/publicSegments/view/b5100771-6c4d-46a4-b3f3-f2fb9a5de5ba/image.png">
-            </div>
-          </dd>
-
-          <h4 style="padding: 10px 0px 20px 0px " id='example-search_experiments'><b>Source code</b>  <small>(Copy it into your python interpreter)</small></h4>
-          <dt>
-            <h5 id='example-search_experiments'>Identification of H3k27ac peaks that overlap with promoters in all BLUEPRINT datasets and subsequent identification of transcription factor peaks that overlap with these promoters, on all ENCODE datasets</h5>
-            <p>We use the <a href="api.php#api-search"</a>search</a> command to find experiments that contain the texts <i>H3k27AC</i>, <i>blood</i>, and <i>peaks</i> in their metadata.
-            <p>We put the names in single quotes to show that these names must be in the metadata.</p>
           </dt>
           <dd>
             <script src="https://gist.github.com/felipealbrecht/058d4fc13adab7f9c146.js"></script>
@@ -175,8 +246,20 @@ include("inc/header.php");
           <h4 id='example-search_experiments'>Use case 3:<br/><b>Calculating the mRNA expression level for your favorite genes across all hematopoietic cell types and subsequent filtering regarding those genes regions where the value of the column named “score” is higher than a given threshold</b></h4>
           <dt style="width:70%">
             <p class="lead" style="text-align: justify; padding: 15px;">
-            blabla
+              We firstly list all experiments that has a sample with the biosource <i>H1-hESC</i>. After, we select the <i>peaks</i> experiments from this list. These experiments regions will be the boundaries of the aggregation that will be performed.
             </p>
+            <p class="lead" style="text-align: justify; padding: 15px;">
+              After, we list all  experiments with <i>liver</i> or <i>hepatocypes</i> biosources.
+            <p>
+            <p class="lead" style="text-align: justify; padding: 15px;">
+              For each listed <i>liver</i> or <i>hepatocytes</i> experiment, we select the associated regions of this experiment.</p>
+            <p>
+            <p class="lead" style="text-align: justify; padding: 15px;">
+              We perform an aggregation with the selected regions using the <i>H1-hESC</i> <i>H3K27ac</i> peaks as boundaries.</p>
+            <p>
+            <p class="lead" style="text-align: justify; padding: 15px;">
+              Finally, we request the regions and download them.
+            <p>
           </dt>
           <dd>
             <div align="right" style="padding: 10px">
