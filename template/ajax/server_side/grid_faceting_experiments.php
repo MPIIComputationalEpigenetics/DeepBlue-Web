8e6b7ec3371629ba07e61bde1301268d6ee81e0d
Obaro Odiete <edeas123@gmail.com>
2016-08-02 18:37:40 +0200

    Only use collection_experiment_count on selected collections: faster  #141

--- template/ajax/server_side/grid_faceting_experiments.php
+++ template/ajax/server_side/grid_faceting_experiments.php
@@ -28,21 +28,77 @@ if (isset($_GET) && isset($_GET["request"])) {
 }
 
 $collection = array();
+$vocabularies_selected = array();
+$vocabularies_unselected = array();
 
-array_key_exists("experiment-project", $filters) ? $collection["projects"] = $filters["experiment-project"] : $collection["projects"] = "";
-array_key_exists("experiment-genome", $filters) ? $collection["genomes"] = $filters["experiment-genome"] : $collection["genomes"] = "";
-array_key_exists("experiment-technique", $filters) ? $collection["techniques"] = $filters["experiment-technique"] : $collection["techniques"] = "";
-array_key_exists("experiment-epigenetic_mark", $filters) ? $collection["epigenetic_marks"] = $filters["experiment-epigenetic_mark"] : $collection["epigenetic_marks"] = "";
-array_key_exists("experiment-biosource", $filters) ? $collection["biosources"] = $filters["experiment-biosource"] : $collection["biosources"] = "";
-array_key_exists("experiment-datatype", $filters) ? $collection["types"] = $filters["experiment-datatype"] : $collection["types"] = "";
-array_key_exists("experiment-sample", $filters) ? $collection["samples"] = $filters["experiment-sample"] : $collection["samples"] = "";
+if(array_key_exists("experiment-project", $filters)) {
+	$collection["projects"] = $filters["experiment-project"];
+	$vocabularies_selected[] = "projects";
+}
+else {
+	$collection["projects"] = "";
+	$vocabularies_unselected[] = "projects";
+}
+
+if(array_key_exists("experiment-genome", $filters)) {
+	$collection["genomes"] = $filters["experiment-genome"];
+	$vocabularies_selected[] = "genomes";
+}
+else {
+	$collection["genomes"] = "";
+	$vocabularies_unselected[] = "genomes";
+}
+
+if(array_key_exists("experiment-technique", $filters)) {
+	$collection["techniques"] = $filters["experiment-technique"];
+	$vocabularies_selected[] = "techniques";
+}
+else {
+	$collection["techniques"] = "";
+	$vocabularies_unselected[] = "techniques";
+}
+
+if(array_key_exists("experiment-epigenetic_mark", $filters)) {
+	$collection["epigenetic_marks"] = $filters["experiment-epigenetic_mark"];
+	$vocabularies_selected[] = "epigenetic_marks";
+}
+else {
+	$collection["epigenetic_marks"] = "";
+	$vocabularies_unselected[] = "epigenetic_marks";
+}
+
+if(array_key_exists("experiment-biosource", $filters)) {
+	$collection["biosources"] = $filters["experiment-biosource"];
+	$vocabularies_selected[] = "biosources";
+}
+else {
+	$collection["biosources"] = "";
+	$vocabularies_unselected[] = "biosources";
+}
+
+if(array_key_exists("experiment-datatype", $filters)) {
+	$collection["types"] = $filters["experiment-datatype"];
+	$vocabularies_selected[] = "types";
+}
+else {
+	$collection["types"] = "";
+	$vocabularies_unselected[] = "types";
+}
+
+if(array_key_exists("experiment-sample", $filters)) {
+	$collection["samples"] = $filters["experiment-sample"];
+	$vocabularies_selected[] = "samples";
+}
+else {
+	$collection["samples"] = "";
+	$vocabularies_unselected[] = "samples";
+}
 
 $client = new IXR_Client(get_server());
 $listInUse = [];
 
 $data = array();
-$vocabularies = ["projects","epigenetic_marks", "biosources", "techniques", "genomes", "samples", "types"];
-foreach ($vocabularies as $vocabulary) {
+foreach ($vocabularies_selected as $vocabulary) {
 	$data[$vocabulary] = array();
 
 	$temp = $collection[$vocabulary];
@@ -70,6 +126,30 @@ foreach ($vocabularies as $vocabulary) {
 	}
 
 	$collection[$vocabulary] = $temp;
+}
+
+if(!$client->query("faceting_experiments", $collection["genomes"], $collection["types"], $collection["epigenetic_marks"],
+	$collection["biosources"], $collection["samples"], $collection["techniques"], $collection["projects"], $user_key)){
+	die('An error occurred - '.$client->getErrorCode().":".$client->getErrorMessage());
+}
+
+$response = $client->getResponse();
+check_error($response);
+
+foreach ($vocabularies_unselected as $vocabulary) {
+	$data[$vocabulary] = array();
+	$data[$vocabulary]['alp'] = $response[1][$vocabulary];
+
+	usort($data[$vocabulary]['alp'], function($a, $b) {
+		return strcasecmp($a[1], $b[1]);
+	});
+
+	if ($vocabulary != "samples") {
+		$data[$vocabulary]['amt'] = $response[1][$vocabulary];
+		usort($data[$vocabulary]['amt'], function($a, $b) {
+			return $a[2] > $b[2];
+		});
+	}
 }
 
 echo json_encode(array($data));
