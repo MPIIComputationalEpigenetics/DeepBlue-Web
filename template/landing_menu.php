5187b335e21f7f0792d6a0f2b2da23fe3caf5095
Felipe Albrecht <felipe.albrecht@gmail.com>
2017-09-13 11:29:55 +0200

    organize landing menu code

--- template/landing_menu.php
+++ template/landing_menu.php
@@ -7,102 +7,65 @@
 		</div>
 
 		<div class="collapse navbar-collapse">
-      		<ul class="nav navbar-nav">
-      			<li class="dropdown">
+      <ul class="nav navbar-nav">
+      	<li class="dropdown">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown">DeepBlue Overview <b class="caret"></b></a>
 					<ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-						<li class="dropdown-header">
-							DeepBlue Overview
-						</li>
+						<li class="dropdown-header">DeepBlue Overview</li>
 						<li class="divider"></li>
-						<li>
-							<a href="deepblue_overview.php">Overview</a>
-						</li>
-						<li>
-							<a href="features.php">Features</a>
-						</li>
-						<li>
-							<a href="citing.php">Citing</a>
-						</li>
-						<li>
-							<a href="acknowledgements.php">Acknowledgements</a>
-						</li>
-						<li>
-							<a href="http://www.mpi-inf.mpg.de/imprint/" target="_blank">Imprint</a>
-						</li>
-				  	</ul>
-        		</li>
+						<li><a href="deepblue_overview.php">Overview</a></li>
+						<li><a href="features.php">Features</a></li>
+						<li><a href="citing.php">Citing</a></li>
+						<li><a href="acknowledgements.php">Acknowledgements</a></li>
+						<li><a href="http://www.mpi-inf.mpg.de/imprint/" target="_blank">Imprint</a></li>
+				  </ul>
+        </li>
 
-      			<li class="dropdown">
+      	<li class="dropdown">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown">API Interface <b class="caret"></b></a>
 					<ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-						<li class="dropdown-header">
-							API Interface
-						</li>
+						<li class="dropdown-header">API Interface</li>
 						<li class="divider"></li>
-						<li>
-							<a href="examples.php">Examples</a>
-						</li>
-						<li>
-							<a href="use_cases.php">Use cases</a>
-						<li>
-							<a href="tutorials.php">Tutorials</a>
-						</li>
-						<li>
-							<a href="manual/" target="_blank">Manual</a>
-						</li>
-						<li>
-							<a href="api.php">API References Commands</a>
-						</li>
+						<li><a href="examples.php">Examples</a></li>
+						<li><a href="use_cases.php">Use cases</a></li>
+						<li><a href="tutorials.php">Tutorials</a></li>
+						<li><a href="manual/" target="_blank">Manual</a></li>
+						<li><a href="api.php">API References Commands</a></li>
 						<li class="divider"></li>
-						<li>
-							<a href="http://deepblue.mpi-inf.mpg.de/R" target="_blank">Accessing from the R Language</a>
-						</li>
-				  	</ul>
-        		</li>
+						<li><a href="http://deepblue.mpi-inf.mpg.de/R" target="_blank">Accessing from the R Language</a></li>
+				  </ul>
+        </li>
 
 
-      			<li class="dropdown">
-  					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Web Interface <b class="caret"></b></a>
+      	<li class="dropdown">
+  				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Web Interface <b class="caret"></b></a>
 					<ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-						<li class="dropdown-header">
-							Web Interface
-						</li>
+						<li class="dropdown-header">Web Interface</li>
 						<li class="divider"></li>
-						<li>
-							<a href="web_examples.php">Examples</a>
-						</li>
-						<li>
-							<a href="<?php echo APP_URL.'/php/deepblue_checkuser.php'?>">Access</a>
-						</li>
-				  	</ul>
-        		</li>
+						<li><a href="web_examples.php">Examples</a></li>
+						<li><a href="<?php echo APP_URL.'/php/deepblue_checkuser.php'?>">Access</a></li>
+				  </ul>
+        </li>
 
-      			<li class="dropdown">
-  					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Feedback <b class="caret"></b></a>
+      	<li class="dropdown">
+  				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Feedback <b class="caret"></b></a>
 					<ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-						<li class="dropdown-header">
-							Feedback
-						</li>
+						<li class="dropdown-header">Feedback</li>
 						<li class="divider"></li>
-						<li>
-							<a href="http://deepblue.userecho.com/">Give your feedback or suggestions</a>
-						</li>
-						<li>
-							<a href="mailto:deepblue@mpi-inf.mpg.de">Write an email to us</a>
-						</li>
-				  	</ul>
-        		</li>
+						<li><a href="http://deepblue.userecho.com/">Give your feedback or suggestions</a></li>
+						<li><a href="mailto:deepblue@mpi-inf.mpg.de">Write an email to us</a></li>
+				  </ul>
+        </li>
 
-        	 </ul>
+			<!-- <ul class="nav navbar-nav"> -->
+      </ul>
 
-		    <ul class="nav navbar-nav navbar-right">
-		    	<li class="active">
-		    		<a href="<?php echo APP_URL; ?>/register.php">Request an account</a>
-				</li>
-		    </ul>
+		  <ul class="nav navbar-nav navbar-right">
+		    <li class="active"><a href="<?php echo APP_URL; ?>/register.php">Request an account</a></li>
+		  </ul>
 
-        </div>
+		<!-- <div class="collapse navbar-collapse"> -->
+  	</div>
 	</div>
 </div>
 
